72afc0830bda987e695acba915cc3814700a4fb5
anhminh4567 <minhtran052003@gmail.com>
2025-02-24 21:37:04 +0700

    init project from milan

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Events/EventResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Evently.Modules.Events.Api.Events;
+
+public sealed record EventResponse(
+    string Id,
+    string Title,
+    string Description,
+    string Location,
+    DateTime StartsAtUtc,
+    DateTime? EndsAtUtc);
